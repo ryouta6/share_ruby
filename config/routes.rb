838ab8945e5133230386649bd8fd0463a923adef
ryouta6 <ryoutaikeda0050@gmail.com>
2024-04-11 15:53:25 +0000

    コミットメント

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/index', to: 'users#index'
+  
+  root 'users#index'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
